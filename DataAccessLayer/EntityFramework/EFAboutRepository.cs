bf040b1c16d816065e9ba2792701ed3bb99615f4
Ali Berk Bostancı <55848455+berkbstnc@users.noreply.github.com>
2022-07-09 01:26:25 +0300

    Initial commit

---
+++ DataAccessLayer/EntityFramework/EFAboutRepository.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EFAboutRepository : GenericRepository<Category>, IAboutDal
+    {
+        public void Delete(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        About IGenericDal<About>.GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<About> IGenericDal<About>.GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/EntityFramework/EFBlogRepository.cs
@@ -0,0 +1,39 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EFBlogRepository : GenericRepository<Blog>, IBlogDal
+    {
+        public void Delete(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category t)
+        {
+            throw new NotImplementedException();
+        }
+
+        Category IGenericDal<Category>.GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Category> IGenericDal<Category>.GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
